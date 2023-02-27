d9ba8da76cd0f6b2b2c01900c1894f120a1c346c
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-02-27 15:52:38 +0100

    structure: scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from "axios";
+import {store} from "./store";
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+components: {
+  AppHeader,
+  data(){
+    return{
+    store,
+    }
+  }
+}
+ }
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+ciao
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './styles/general.scss' as *;
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+  name:"AppHeader",
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
